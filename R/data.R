cd179dbcbecee435ce065dd1d10e5abd69690e34
Corey Broeckling <cbroeckl@users.noreply.github.com>
2025-07-17 12:53:21 -0600

    trying to get vignette to recognize data files

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' A small dataset of a pubchem.bio metabolome scored by taxon, for inclusion in vignette
+#'
+#' @name pc.bio.tax.scored.subset.Rda
+#' @docType .Rda
+#' @author Corey Broeckling
+#' @keywords pubchem.bio data
+NULL
+
+#' A small dataset of a the taxonomy hierarchy, for inclusion in vignette
+#'
+#' @name sub.taxid.hierarchy.Rda
+#' @docType .Rda
+#' @author Corey Broeckling
+#' @keywords pubchem.bio data
+NULL
