3253928d03f1cff4ebec9a742364357716f28599
Corey Broeckling <4959997+cbroeckl@users.noreply.github.com>
2025-07-24 16:20:00 -0600

    Update data.R

--- R/data.R
+++ R/data.R
@@ -14,14 +14,144 @@ NULL
 
 #' taxid.hierarchy.rda
 #' @name taxid.hierarchy
-#' @description A random subset of the full taxonomy hierarchy, for testing purposes
+#' @description A subset of the full taxonomy hierarchy, for testing purposes
 #' @format data.table, stored in .rda format
 #' @source pubchem.bio data.table output from NCBI Taxonomy data
 NULL
 
 #' cid.taxid.rda
 #' @name taxid.hierarchy
-#' @description A random subset of the full cid.taxid, for testing purposes
+#' @description A subset of the full cid.taxid, for testing purposes
 #' @format data.table, stored in .rda format
 #' @source subset of cid.taxid file from get.pubchem.ftp
 NULL
+
+#' cid.accurate.mass.rda
+#' @name taxid.hierarchy
+#' @description A subset of the full cid.taxid, for testing purposes
+#' @format data.table, stored in .rda format
+#' @source subset of cid.taxid file from get.pubchem.ftp
+NULL
+
+#' cid.cas.rda
+#' @name taxid.hierarchy
+#' @description A subset of the full cid.taxid, for testing purposes
+#' @format data.table, stored in .rda format
+#' @source subset of cid.taxid file from get.pubchem.ftp
+NULL
+
+#' cid.formula.rda
+#' @name taxid.hierarchy
+#' @description A subset of the full cid.taxid, for testing purposes
+#' @format data.table, stored in .rda format
+#' @source subset of cid.taxid file from get.pubchem.ftp
+NULL
+
+#' cid.inchi.rda
+#' @name taxid.hierarchy
+#' @description A subset of the full cid.taxid, for testing purposes
+#' @format data.table, stored in .rda format
+#' @source subset of cid.taxid file from get.pubchem.ftp
+NULL
+
+#' cid.inchikey.rda
+#' @name taxid.hierarchy
+#' @description A subset of the full cid.taxid, for testing purposes
+#' @format data.table, stored in .rda format
+#' @source subset of cid.taxid file from get.pubchem.ftp
+NULL
+
+#' cid.lca.rda
+#' @name taxid.hierarchy
+#' @description A subset of the full cid.taxid, for testing purposes
+#' @format data.table, stored in .rda format
+#' @source subset of cid.taxid file from get.pubchem.ftp
+NULL
+
+#' cid.mesh.function.rda
+#' @name taxid.hierarchy
+#' @description A subset of the full cid.taxid, for testing purposes
+#' @format data.table, stored in .rda format
+#' @source subset of cid.taxid file from get.pubchem.ftp
+NULL
+
+#' cid.mesh.name.rda
+#' @name taxid.hierarchy
+#' @description A subset of the full cid.taxid, for testing purposes
+#' @format data.table, stored in .rda format
+#' @source subset of cid.taxid file from get.pubchem.ftp
+NULL
+
+#' cid.monoisotopic.mass.rda
+#' @name taxid.hierarchy
+#' @description A subset of the full cid.taxid, for testing purposes
+#' @format data.table, stored in .rda format
+#' @source subset of cid.taxid file from get.pubchem.ftp
+NULL
+
+#' cid.parent.rda
+#' @name taxid.hierarchy
+#' @description A subset of the full cid.taxid, for testing purposes
+#' @format data.table, stored in .rda format
+#' @source subset of cid.taxid file from get.pubchem.ftp
+NULL
+
+#' cid.pmid.rda
+#' @name taxid.hierarchy
+#' @description A subset of the full cid.taxid, for testing purposes
+#' @format data.table, stored in .rda format
+#' @source subset of cid.taxid file from get.pubchem.ftp
+NULL
+
+#' cid.pmid.ct.rda
+#' @name taxid.hierarchy
+#' @description A subset of the full cid.taxid, for testing purposes
+#' @format data.table, stored in .rda format
+#' @source subset of cid.taxid file from get.pubchem.ftp
+NULL
+
+#' cid.preferred.rda
+#' @name taxid.hierarchy
+#' @description A subset of the full cid.taxid, for testing purposes
+#' @format data.table, stored in .rda format
+#' @source subset of cid.taxid file from get.pubchem.ftp
+NULL
+
+#' cid.pwid.rda
+#' @name taxid.hierarchy
+#' @description A subset of the full cid.taxid, for testing purposes
+#' @format data.table, stored in .rda format
+#' @source subset of cid.taxid file from get.pubchem.ftp
+NULL
+
+#' cid.sid.rda
+#' @name taxid.hierarchy
+#' @description A subset of the full cid.taxid, for testing purposes
+#' @format data.table, stored in .rda format
+#' @source subset of cid.taxid file from get.pubchem.ftp
+NULL
+
+#' cid.smiles.rda
+#' @name taxid.hierarchy
+#' @description A subset of the full cid.taxid, for testing purposes
+#' @format data.table, stored in .rda format
+#' @source subset of cid.taxid file from get.pubchem.ftp
+NULL
+
+#' cid.synonym.rda
+#' @name taxid.hierarchy
+#' @description A subset of the full cid.taxid, for testing purposes
+#' @format data.table, stored in .rda format
+#' @source subset of cid.taxid file from get.pubchem.ftp
+NULL
+
+#' cid.title.rda
+#' @name taxid.hierarchy
+#' @description A subset of the full cid.taxid, for testing purposes
+#' @format data.table, stored in .rda format
+#' @source subset of cid.taxid file from get.pubchem.ftp
+NULL
+
+
+
+
