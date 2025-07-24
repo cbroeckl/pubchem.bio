665687f05140292d46819a2e4d0cd59403fdf5ed
Corey Broeckling <4959997+cbroeckl@users.noreply.github.com>
2025-07-24 15:06:14 -0600

    add example data files, documentation

--- R/data.R
+++ R/data.R
@@ -11,3 +11,17 @@ NULL
 #' @format data.table, stored in .rda format
 #' @source pubchem.bio data.table output from NCBI Taxonomy data
 NULL
+
+#' taxid.hierarchy.rda
+#' @name taxid.hierarchy
+#' @description A random subset of the full taxonomy hierarchy, for testing purposes
+#' @format data.table, stored in .rda format
+#' @source pubchem.bio data.table output from NCBI Taxonomy data
+NULL
+
+#' cid.taxid.rda
+#' @name taxid.hierarchy
+#' @description A random subset of the full cid.taxid, for testing purposes
+#' @format data.table, stored in .rda format
+#' @source subset of cid.taxid file from get.pubchem.ftp
+NULL
